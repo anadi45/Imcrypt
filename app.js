72a761094221cf5703508cbfe70b745661d3b1f6
anadi45 <ranjeetbaraik15@gmail.com>
2022-07-30 01:25:59 +0530

    Frontend added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
+app.use(express.static('client/build'));
+
 const encodeRoute = require("./routes/encodeRoute");
 const decodeRoute = require("./routes/decodeRoute");
 
